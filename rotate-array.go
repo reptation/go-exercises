c16a805fd9cf1a1776af6bb74fbf82ef0c71fe46
David Welch <david@musicalquilt.com>
2023-11-28 22:40:24 +0000

    act on original array

--- rotate-array.go
+++ rotate-array.go
@@ -5,11 +5,14 @@ import (
 )
 
 func rotate(nums []int, k int) {
-    var rotatedNums = make([]int, len(nums))
-    copy(rotatedNums[:k], nums[len(nums)-k:])
-    copy(rotatedNums[k+1:], nums[:len(nums)-k])
-
-    nums = rotatedNums
+    // If k is greater or equal than len(nums) there are problems
+    if k >= len(nums) {
+        k = k % len(nums)
+    }
+    var dupeNums = make([]int, len(nums))
+    copy(dupeNums, nums)
+    copy(nums[:k], dupeNums[len(dupeNums)-k:])
+    copy(nums[k:], dupeNums[:len(dupeNums)-k])
     fmt.Println(nums)
 }
 
@@ -17,4 +20,20 @@ func main() {
     a := []int{1,2,3,4,5,6,7}
     k := 3
     rotate(a, k)
+
+    a = []int{1}
+    k = 1
+    rotate(a,k)
+
+    a = []int{1,2}
+    k = 2
+    rotate(a, k)
+
+    a = []int{-1}
+    k = 2
+    rotate(a,k)
+
+    a = []int{1,2,3,4,5,6}
+    k = 1
+    rotate(a,k)
 }
